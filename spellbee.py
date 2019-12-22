d5ea0bfd0dcda18403b6b0bf4024044cc2159ca6
Jay Talreja <jay.talreja@macefusion.com>
2019-12-21 19:53:45 -0500

    School List + bug fixes

--- spellbee.py
+++ spellbee.py
@@ -4,25 +4,30 @@ import time
 import datetime
 import random
 import subprocess
-f=open('2019SpellitList.csv')
+#f=open('2019SpellitList.csv')
+f=open('SixthGrade.txt')
 words = f.read()
 words= words.split('\n')
 words = [word for word in words if len(word)> 1 and word[0] != '%']
+words = words[84:]
 fw=open('remember_words.csv','a+')
 fw.write('*************************************************\n')
 fw.write('Spelling Practice on : %s'%datetime.datetime.now())
 fw.write('\n')
 correctWords=[]
 incorrectWords = []
-
+INDEX=0
 def sayWord(word):
   speech="osascript -e 'say \""+word+"\"'"
   system(speech)
 
 def chooseWord(upperBound): 
   #word=words[random.randint(0,len(words)-1)]
-  word=words[random.randint(0,upperBound)]
-  return word
+  word=words[upperBound]
+  #word=words[random.randint(0,upperBound)]
+  #word=words[random.randint(389,425)]
+
+  return word.strip()
 
 def showDialog(word,showWord=False):
   sayWord(word)
@@ -37,13 +42,14 @@ def showDialog(word,showWord=False):
   return output
 
 
-def chooseAndSayWord(upperBound,word,correctWords,incorrectWords):
-  if not word:
-    word = chooseWord(upperBound)
+def chooseAndSayWord(idx,word,correctWords,incorrectWords):
+  word=chooseWord(idx)
+  #if not word:
+  #  word = chooseWord(upperBound)
   output = showDialog(word)
   if "Repeat" in output:
      sayWord("Repeating the word")
-     chooseAndSayWord(upperBound,word,correctWords,incorrectWords)
+     chooseAndSayWord(idx,word,correctWords,incorrectWords)
   if "Show" in output:
      incorrectWords.append(word)
      sayWord('%s is spelled as follows'%word)
@@ -58,15 +64,19 @@ def chooseAndSayWord(upperBound,word,correctWords,incorrectWords):
      #fw.write(word)
      #fw.write('\n')
      sayWord("Incorrect spelling, please try again")
-     chooseAndSayWord(upperBound,word,correctWords,incorrectWords)
+     chooseAndSayWord(idx,word,correctWords,incorrectWords)
   if "Check" in output and word.upper() in output.upper():
      correctWords.append(word)
 
 if __name__=="__main__":
+   import sys
    args=sys.argv
+   #sayWord(args[1]) 
+   #sys.exit(0)
+   sayWord("Anya is a good girl. Aggi is a good boy.")
    totalWordsToChoose=int(args[1])
    for i in xrange(totalWordsToChoose):
-     chooseAndSayWord(250,"",correctWords,incorrectWords)
+     chooseAndSayWord(i,"",correctWords,incorrectWords)
    percentCorrect=round(float(len(correctWords))/float(totalWordsToChoose)*100)
    percentCorrect=int(percentCorrect)
    sayWord('You spelled %s words correctly out of %s attempted with %s correct percent'%(len(correctWords),totalWordsToChoose,percentCorrect))
